a736acc36d4fa89d4869c7d0fd5daebd9f27c96f
Michel Barbou Salvador <mibarbou@gmail.com>
2016-05-02 23:05:17 +0200

    con servicio para obtener la lista de anuncios, con callback

--- models/Anuncio.js
+++ models/Anuncio.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 var anuncioSchema = mongoose.Schema(
     { nombre: String,
@@ -9,3 +10,30 @@ var anuncioSchema = mongoose.Schema(
         foto: String,
         tags: [String]
 });
+
+// Hacer método estático
+anuncioSchema.statics.list = function (filter, start, limit, sort, cb) {
+
+        var query = Anuncio.find(filter);
+        query.skip(start);
+        query.limit(limit);
+        query.sort(sort);
+        return query.exec(cb);
+};
+
+// anuncioSchema.statics.list = function () {
+//
+//         return new Promise(function (resolve, reject) {
+//
+//                 fs.readFile(__dirname + '/../anunciosMock.json', 'utf-8', function (err, data) {
+//
+//                         if(err) {
+//                                
+//                                 return reject(err);
+//                         }
+//                         return resolve(JSON.parse(data));
+//                 });
+//         });
+// };
+
+var Anuncio = mongoose.model('Anuncio', anuncioSchema);
--- routes/apiv1/anuncios.js
+++ routes/apiv1/anuncios.js
@@ -1,3 +1,34 @@
-/**
- * Created by michel on 02/05/16.
- */
+"use strict";
+
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Anuncio = mongoose.model('Anuncio');
+
+//var jwtAuth = require('../../lib/jwtAuth');
+
+//router.use(jwtAuth());
+
+router.get('/', function (req, res, next) {
+    var name = req.query.name;
+    var start = parseInt(req.query.start) || 0;
+    var limit = parseInt(req.query.limit) || null;
+    var sort = req.sort || null;
+
+    var criteria = {};
+
+    if (typeof  name !== 'undefined'){
+        criteria.name = name;
+    }
+
+    Anuncio.list(criteria, start, limit, sort, function (err, rows) {
+
+        if (err) {
+            return res.json({success: false, error: err});
+        }
+        res.json({success: true, rows: rows});
+    });
+});
+
+module.exports = router;
